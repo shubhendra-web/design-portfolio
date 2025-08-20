0fc7f5576ca893605859c91846c62740d8b767d8
shubhendra -web <shubhandersinghrhathore2000@gmail.com>
2025-08-20 05:34:20 +0530

    updated code

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("hidden");
+}
+
+function toggleMenu() {
+  const nav = document.getElementById("navMenu");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
